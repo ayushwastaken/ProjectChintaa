3cf43297f70047546546e15c713ad79d29c5accd
ayushwastaken <90954626+ayushwastaken@users.noreply.github.com>
2025-03-05 00:32:20 +0530

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,52 @@
+
+/*
+document.addEventListener('DOMContentLoaded', function(){
+    fetch('/posts')
+    .then((response) => response.json())
+    .then((posts) => {
+        const postContainer = document.getElementById("posts");
+        postContainer.innerHTML = posts
+        .map(
+            (post) => `
+            <div class="post ${post.category}">
+               ${
+                post.image ? `<img class="post-img" src="${post.image}" alt="${post.title}">` : ""
+               }
+               <h3 class="post-category">${post.category}</h3>
+               <a href="/post-details/${encodeURIComponent(
+                    post.title.trim().replace(/\s+/g, "-").toLowerCase()
+               )}">
+                <h2 class="post-title">${post.title}</h2>
+                </a>
+                <div class="post-desc">${post.content}</div>
+               <a href="/post-details/${encodeURIComponent(
+                    post.title.trim().replace(/\s+/g, "-").toLowerCase()
+               )}" class="read-more">Read More</a>
+               </div>
+            `
+        )
+        .join("");
+        })
+        .catch((error) => {
+            console.error('Error fetching posts:', error);
+            const postContainer = document.getElementById("posts");
+            postContainer.innerHTML = '<p>Error fetching posts.</p>';
+        });
+});
+
+//Header
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0);
+});*/
+
+window.addEventListener("scroll", function () {
+    let userImg = document.getElementById("user-img");
+    if (window.scrollY > 50) { // Adjust value as needed
+        userImg.style.opacity = "0";
+        userImg.style.transition = "opacity 0.3s ease-in-out";
+    } else {
+        userImg.style.opacity = "1";
+    }
+});
